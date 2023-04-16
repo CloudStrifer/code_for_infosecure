41137a8c9de6385b44f70d942806c548ec915258
LAPTOP-7NL1FFB8\Cloud <634133930@163.com>
2023-04-16 22:08:11 +0800

    修改部分代码

--- Flow_extract/isxc-vpn/CNN_for_traffic_classification_LeNet.py
+++ Flow_extract/isxc-vpn/CNN_for_traffic_classification_LeNet.py
@@ -6,7 +6,11 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D,AveragePooling2D
 from pathlib import Path
 import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from sklearn.model_selection import train_test_split
 from prefix import PREFIX_TO_TRAFFIC_ID, ID_TO_TRAFFIC
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 # 全局变量
 batch_size = 128  # 批处理样本数量
 nb_classes = 10  # 分类数目
@@ -28,7 +32,6 @@ def get_training_data_set():
         row, col = 1500, 1500
         flow_picture_data = [[0 for _ in range(col)] for _ in range(row)]
 
-        #picture = []
         for i in range(len(df)):
                 bidirectional_mean_ps = int(df.at[i,'bidirectional_mean_ps'])
                 bidirectional_duration_ms = int(df.at[i,'bidirectional_duration_ms'])
@@ -44,22 +47,24 @@ def get_training_data_set():
     return flow_dataset, flow_picture_label   # 图片为[[x,x,x..][x,x,x...][x,x,x...][x,x,x...]]的列表
 
 datasets, labels = get_training_data_set()
-print("1")
 
+#划分训练集和测试集
+X_train, X_test, Y_train, Y_test = train_test_split(
+    datasets, labels, test_size=0.30, random_state=42)
 
-# X_train, Y_train = MNIST.get_training_data_set(600, False)  # 加载训练样本数据集，和one-hot编码后的样本标签数据集。最大60000
-# X_test, Y_test = MNIST.get_test_data_set(100, False)  # 加载测试特征数据集，和one-hot编码后的测试标签数据集，最大10000
-X_train = datasets
-Y_train = labels
+#X_train = datasets
+#Y_train = labels
 
-X_test = datasets
-Y_test = labels
+#X_test = datasets
+#Y_test = labels
 
-X_train = np.array(X_train).astype(bool).astype(float)/255    #数据归一化
-X_train=X_train[:,:,:,np.newaxis]  # 添加一个维度，代表图片通道。这样数据集共4个维度，样本个数、宽度、高度、通道数
+#X_train = np.array(X_train).astype(bool).astype(float)/255    #数据归一化
+X_train = np.array(X_train)
+X_train = X_train[:,:,:,np.newaxis]  # 添加一个维度，代表图片通道。这样数据集共4个维度，样本个数、宽度、高度、通道数
 Y_train = np.array(Y_train)
-X_test = np.array(X_test).astype(bool).astype(float)/255    #数据归一化
-X_test=X_test[:,:,:,np.newaxis]  # 添加一个维度，代表图片通道。这样数据集共4个维度，样本个数、宽度、高度、通道数
+#X_test = np.array(X_test).astype(bool).astype(float)/255    #数据归一化
+X_test = np.array(X_test)
+X_test = X_test[:,:,:,np.newaxis]  # 添加一个维度，代表图片通道。这样数据集共4个维度，样本个数、宽度、高度、通道数
 Y_test = np.array(Y_test)
 print('样本数据集的维度：', X_train.shape,Y_train.shape)
 print('测试数据集的维度：', X_test.shape,Y_test.shape)
@@ -79,10 +84,31 @@ model.summary()
 # 编译模型
 model.compile(loss='SparseCategoricalCrossentropy',optimizer='adadelta',metrics=['accuracy'])
 # 训练模型
-model.fit(X_train, Y_train, batch_size=batch_size, epochs=5,verbose=1, validation_data=(X_test, Y_test))
+history = model.fit(X_train, Y_train, batch_size=batch_size, epochs=5,verbose=1, validation_data=(X_test, Y_test))
 # 评估模型
 score = model.evaluate(X_test, Y_test, verbose=0)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(len(acc))
+
+plt.plot(epochs,acc, 'b', label='Training accuracy')
+plt.plot(epochs, val_acc, 'r', label='validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend(loc='lower right')
+plt.figure()
+
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+#plt.savefig("accuracy_loss.jpg")
+#os.system("pause")
 # 保存模型
-model.save('cnn_model.h5')   # HDF5文件, pip install h5py
+#model.save('cnn_model.h5')   # HDF5文件, pip install h5py
+
